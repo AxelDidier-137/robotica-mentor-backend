0e660beedb982578af84393618ee331f2aa87945
AxelDidier-137 <lostlemur1818@gmail.com>
2026-04-29 20:42:44 -0600

    fix modelo groq

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.post("/chat", async (req, res) => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "llama-3.1-8b-instant",
+        mmodel: "llama-3.1-8b-instant",
         messages: [
           {
             role: "system",
